f6c9cb7b76f6c242c5144d5213f40f1e258fe392
Mouktik Ghosh <27.mghosh@gmail.com>
2021-07-21 01:05:17 +0530

    api-gateway commited

---
+++ songs-api-gateway-service/src/test/java/com/micro/songsapigatewayservice/SongsApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.songsapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
